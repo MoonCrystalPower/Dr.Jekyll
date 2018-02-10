0af0ca4bab50acf4446718b1ca131a2d1c19adcf
byeolbit <yeonwoo.jo.92@gmail.com>
2018-02-10 22:57:54 +0900

    Add no-sandbox option

--- karma.conf.js
+++ karma.conf.js
@@ -37,7 +37,13 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['ChromeNoSandbox'],
+    customLaunchers: {
+      ChromeNoSandbox: {
+        base: 'Chrome',
+        flags: ['--no-sandbox']
+      }
+    },
     singleRun: true,
   });
 };
